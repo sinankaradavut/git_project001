a0bbf627ed5a355590957352fe6d1513f472fea7
Sinan Karadavut <sinankaradavut84@gmail.com>
2020-08-08 14:32:52 -0400

    last commit from local master branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -6,5 +6,8 @@ public class FirstClass {
 		System.out.println("Our first commit from local repo");
 		System.out.println("Our second commit from local repository");
 		System.out.println("Now I am on my sinan branch");
+		System.out.println("I want to have conflict");
+		
+		System.out.println("I want to have a second conflict");
 	}
 }
